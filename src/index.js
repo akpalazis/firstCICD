a154d112735046b8dad5b546f2c058d9c0773632
akpalazis <palazis@ribopro.eu>
2024-02-23 18:15:16 +0100

    Refactored the username validation

--- src/index.js
+++ src/index.js
@@ -9,17 +9,14 @@ app.post('/login', async (req, res) => {
     const username = req.body.username
     console.log(username)
     if (username) {
-      if (username !== "") {
-        // TODO: Check for username in db return successful 200 or Username not found 404
-        res.status(200).send('Login Successful');
-      }
-    } else {
-        throw new Error("Empty Username Field")
-      }
+      res.status(200).send('Login Successful');
+    }
+    res.status(400).send('Empty Username Field')
   } catch (err) {
     res.status(400).send(err.message);
   }
 });
+
 
 const port = process.env.PORT || 3000;
 const address = app.listen(port, function() {

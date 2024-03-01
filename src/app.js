901b2312c42884369b7c5f3bd3b4622a120106e8
akpalazis <palazis@ribopro.eu>
2024-03-01 13:46:01 +0100

    Check jenkins for env var

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use('/',token);
 
 // Use the imported files for different endpoints
 
-
+console.log(process.env.JENKINS)
 const PORT = process.env.PORT || 3000;
 const address = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

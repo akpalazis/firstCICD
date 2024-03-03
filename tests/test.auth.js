60372878f403c6adcd559f149d1af579f10b7e0f
akpalazis <palazis@ribopro.eu>
2024-03-03 03:01:36 +0100

    Added more test for the auth functions

--- tests/test.auth.js
+++ tests/test.auth.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const { expect } = require('expect');
 const {isJenkins,HOST_URL} = require('../src/constants')
 const {validateData} = require('../src/auth-tools')
+const {userDatabaseTools} = require('../src/auth-db-tools')
 
 let app;
 
@@ -11,7 +12,7 @@ if (isJenkins){
   app = require('../src/app.js');
 }
 describe('Test validateData', () => {
-  it('No Username and password: responds with invalid status code', async () => {
+  it('No Username and password: responds with error message', async () => {
     try {
       const credentials = {};
       await validateData(credentials);
@@ -19,135 +20,75 @@ describe('Test validateData', () => {
       expect(e.message).toBe('Username and Password field not found') // Check for expected status code
     }
   })
-})
+  it('No Username: responds with error message', async () => {
+    try {
+      const credentials = {password: "1234"}
+      await validateData(credentials)
+    } catch (e) {
+      expect(e.message).toBe('Username field not found') // Check for expected status code
+    }
+  });
+  it('No Password: responds with error message', async () => {
+    try {
+      const credentials = {username: "1234"}
+      await validateData(credentials)
+    } catch (e) {
+      expect(e.message).toBe('Password field not found') // Check for expected status code
+    }
+  });
+  it('Empty username and password: responds with error message', async () => {
+    try {
+      const credentials = {username: "", password: ""}
+      await validateData(credentials)
+    } catch (e) {
+      expect(e.message).toBe('Empty Username and Password Field') // Check for expected status code
+    }
+  });
+  it('Empty username: responds with error message', async () => {
+    try {
+      const credentials = {username: "", password: "1234"}
+      await validateData(credentials)
+    } catch (e) {
+      expect(e.message).toBe('Empty Username Field')
+    }
 
-describe('Testing POST /validate endpoint', () => {
-  it('No Username and password: responds with invalid status code', () => {
-    return request(app)
-      .post('/validate') // Specify the POST method
-      .send({}) // Attach username and password in the request body
-      .then((response) => {
-        expect(response.status).toBe(400); // Check for expected status code
-        expect(response.text).toBe('Username and Password field not found') // Check for expected status code
-      })
-      .catch((err) => {
-        return done(err); // Handle potential errors
-      });
   });
-  it('No Username: responds with invalid status code', () => {
-    return request(app)
-      .post('/validate') // Specify the POST method
-      .send({password: "1234"}) // Attach username and password in the request body
-      .then((response) => {
-        expect(response.status).toBe(400); // Check for expected status code
-        expect(response.text).toBe('Username field not found') // Check for expected status code
-      })
-      .catch((err) => {
-        return done(err); // Handle potential errors
-      });
+  it('Empty password: responds with error message', async () => {
+    try {
+      const credentials = {username: "admin", password: ""}
+      await validateData(credentials)
+    } catch (e) {
+      expect(e.message).toBe('Empty Password Field') // Check for expected status code
+    }
   });
-  it('No Password: responds with invalid status code', () => {
-    return request(app)
-      .post('/validate') // Specify the POST method
-      .send({username: "1234"}) // Attach username and password in the request body
-      .then((response) => {
-        expect(response.status).toBe(400); // Check for expected status code
-        expect(response.text).toBe('Password field not found') // Check for expected status code
-      })
-      .catch((err) => {
-        return done(err); // Handle potential errors
-      });
-  });
-  it('Empty username and password: responds with invalid status code', () => {
-    return request(app)
-      .post('/validate')
-      .send({username:"", password:""})// Specify the POST method
-      .then((response) => {
-        expect(response.status).toBe(400); // Check for expected status code
-        expect(response.text).toBe('Empty Username and Password Field') // Check for expected status code
-
-      })
-      .catch((err) => {
-        return done(err); // Handle potential errors
-      });
-  });
-  it('Empty username: responds with invalid status code', () => {
-    return request(app)
-      .post('/validate')
-      .send({username:"", password:"1234"})// Specify the POST method
-      .then((response) => {
-        expect(response.status).toBe(400); // Check for expected status code
-        expect(response.text).toBe('Empty Username Field') // Check for expected status code
-
-      })
-      .catch((err) => {
-        return done(err); // Handle potential errors
-      });
-  });
-  it('Empty password: responds with invalid status code', () => {
-    return request(app)
-      .post('/validate')
-      .send({username:"admin", password:""})// Specify the POST method
-      .then((response) => {
-        expect(response.status).toBe(400); // Check for expected status code
-        expect(response.text).toBe('Empty Password Field') // Check for expected status code
-
-      })
-      .catch((err) => {
-        return done(err); // Handle potential errors
-      });
-  });
-  it('Successful Validation: responds with valid status code', () => {
-    return request(app)
-      .post('/validate')
-      .send({username:"admin", password:"1234"})// Specify the POST method
-      .then((response) => {
-        expect(response.status).toBe(200); // Check for expected status code
-        expect(response.text).toBe('Valid User') // Check for expected status code
-
-      })
-      .catch((err) => {
-        return done(err); // Handle potential errors
-      });
+  it('Successful Validation: responds with undefined', async () => {
+      const credentials = {username: "admin", password: "1234"}
+      const validation = await validateData(credentials)
+      expect(validation).toBeUndefined()
   });
 });
 
-describe('Testing POST /login endpoint', () => {
-  it('Login Successful: responds with valid status code', () => {
-    return request(app)
-      .post('/login') // Specify the POST method
-      .send({ username: "admin",password:"pass"}) // Attach username and password in the request body
-      .then((response) => {
-        expect(response.status).toBe(200);
-        expect(response.text).toBe('Login Successful') // Check for expected status code
-      })
-      .catch((err) => {
-        return done(err); // Handle potential errors
-      });
+describe('Testing userDatabaseTools.isValidUser', () => {
+  it('Login Successful: responds with undefined', async () => {
+    const credentials = { username: "admin",password:"pass"}
+    const validUser = await userDatabaseTools.isValidUser(credentials)
+    expect(validUser).toBeUndefined()
   });
-  it('Incorrect Password: responds with valid status code', () => {
-    return request(app)
-      .post('/login') // Specify the POST method
-      .send({ username: "admin",password:"passs"}) // Attach username and password in the request body
-      .then((response) => {
-        expect(response.status).toBe(400);
-        expect(response.text).toBe('Username does not match with password') // Check for expected status code
-      })
-      .catch((err) => {
-        return done(err); // Handle potential errors
-      });
+  it('Incorrect Password: responds with error message', async () => {
+    try {
+      const credentials = {username: "admin", password: "passs"}
+      await userDatabaseTools.isValidUser(credentials)
+    } catch (e) {
+      expect(e.message).toBe("Username does not match with password")
+    }
   });
-  it('User doesnt exists: responds with invalid status code', () => {
-    return request(app)
-      .post('/login') // Specify the POST method
-      .send({ username: "invalid_user" , password: "1234"}) // Attach username and password in the request body
-      .then((response) => {
-        expect(response.status).toBe(400);
-        expect(response.text).toBe('Username not Found') // Check for expected status code
-      })
-      .catch((err) => {
-        return done(err); // Handle potential errors
-      });
+  it('User doesnt exists: responds with error message', async () => {
+    try {
+      const credentials = {username: "invalid_user", password: "1234"}
+      await userDatabaseTools.isValidUser(credentials)
+    } catch (e) {
+      expect(e.message).toBe('Username not Found')
+    }
   });
 });
 

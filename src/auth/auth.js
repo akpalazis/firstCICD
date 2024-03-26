206d9056fe84f80360279d0f2c0b0ccfcddcb007
akpalazis <palazis@ribopro.eu>
2024-03-26 23:42:19 +0100

    Added sign url function to reset the password

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const authRouter = express.Router();
+const {createHmac} = require('crypto');
 const {
   dataValidationMiddleware,
   generateHashMiddleware,
@@ -9,6 +10,7 @@ const {
   canDeleteMiddleware,
   deleteUserMiddleware,
   fetchTokenMiddleware} = require('./auth-middleware')
+const {AUTH_SECRET_KEY,HOST_URL} = require("../constants")
 
 const {roleManager,storeTokens,validateTokenMiddleware,allowLoginUsersMiddleware} = require("../commonMiddleware")
 
@@ -77,6 +79,60 @@ authRouter.get('/clearCookies', (req, res) => {
 });
 
 
+
+function generateSignedURL(username) {
+  const url = `/reset-password`
+  const timestamp = Math.floor(Date.now() / 1000); // Current timestamp in seconds
+  const expiryTimestamp = timestamp + 600;
+
+  const signature = createHmac('sha256', AUTH_SECRET_KEY)
+                             .update(`${url}?user=${username}&expiry=${expiryTimestamp}`)
+                             .digest('hex');
+    return `${url}?user=${username}&expiry=${expiryTimestamp}&signature=${signature}`;
+}
+
+
+function verifySignedURL(params) {
+    const currentTimestamp = Math.floor(Date.now() / 1000);
+    if (currentTimestamp > params.expiry) {
+        return false; // URL has expired
+    }
+    const expectedSignature = createHmac('sha256', AUTH_SECRET_KEY)
+                                   .update(`/reset-password?user=${params.user}&expiry=${params.expiry}`)
+                                   .digest('hex');
+    return params.signature === expectedSignature;
+}
+
+function middlewareSignedURL(req, res, next) {
+    if (Object.keys(req.query).length === 0) {
+      const signed = generateSignedURL("admin")
+      res.locals.signed = true
+      res.locals.url = req.protocol + '://' + req.headers.host + signed
+    }else {
+      res.locals.signed = false
+    }
+    return next()
+}
+
+function middlewareVerifySignedURL(req,res,next){
+  if (Object.keys(req.query).length !== 0) {
+    if(verifySignedURL(req.query)){
+      next()
+    }
+    return res.status(400).send("Invalid url");
+  }
+  return next()
+}
+
+authRouter.get('/reset-password',
+  middlewareSignedURL,
+  middlewareVerifySignedURL,
+  async (req,res)=>{
+  if(res.locals.signed) {
+    return res.status(200).send(res.locals.url);
+  }
+  return res.status(200).send("Password changed successfully");
+})
 module.exports = {authRouter}
 
 
